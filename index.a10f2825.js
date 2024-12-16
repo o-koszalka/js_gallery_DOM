0a4dafb66ad93e60b9429dc1654940d0d2ade00a
Oskar Koszalka <oskar.koszalka@wp.pl>
2024-12-16 15:46:57 +0100

    make build

---
+++ index.a10f2825.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){var n=t.target;if("IMG"===n.tagName||"A"===n.tagName){t.preventDefault();var a="IMG"===n.tagName?n.parentNode.href:n.href;e.src=a}})});
+//# sourceMappingURL=index.a10f2825.js.map
